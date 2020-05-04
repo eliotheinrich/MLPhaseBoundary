530322f21984c2ecb74954e9154d3f6a2d5599cb
eliotheinrich <eliot.heinrich@gmail.com>
2020-05-04 12:49:46 -0400

    Fixed GaugeIsing2D

--- config.py
+++ config.py
@@ -359,36 +359,43 @@ class GaugeIsing2D:
         self.I = I
         self.T = T
         self.s = np.random.choice([-1,1], size=(L,L))
-        self.s[1::2,::2] = 0
+
+        # Sites corresponding to corners or plaquettes
         self.s[::2,1::2] = 0
+        self.s[1::2,::2] = 2
+        
+        # Indices of plaquettes
+        self.p = [(i,j) for i in range(1,L,2) for j in range(0,L,2)]
 
     # Computes internal energy of system
     def E(self):
         E = 0.
-        for i in range(0, self.L):
-            for j in range(i%2-1, self.L, 2):
-                Q = 0
-                Q *= self.s[i][(j-1)%self.L] \
-                   * self.s[i][(j+1)%self.L] \
-                   * self.s[(i-1)%self.L][j] \
-                   * self.s[(i+1)%self.L][j] \
-
-                E += Q
+        for p in self.p:
+            i,j = p
+            E += self.s[i][(j-1)%self.L]*self.s[i][(j+1)%self.L] \
+               * self.s[(i-1)%self.L][j]*self.s[(i+1)%self.L][j]
 
         return self.I*E
 
     # Computes change in internal energy from flipping spin at site (i,j)
     def dE(self, i, j):
         s1 = self.s[i][j]
-        s2 = self.s[(i-1)%self.L][(j+1)%self.L]
-        s3 = self.s[i][(j+2)%self.L]
-        s4 = self.s[(i+1)%self.L][(j+1)%self.L]
-        s5 = self.s[(i+2)%self.L][j]
-        s6 = self.s[(i+1)%self.L][(j-1)%self.L]
-        s7 = self.s[i][(j-2)%self.L]
-        s8 = self.s[(i-1)%self.L][(j-1)%self.L]
-        s9 = self.s[(i-2)%self.L][j]
-        dE = 2*self.I*s1*(s2*s3*s4 + s4*s5*s6 + s6*s7*s8 + s8*s9*s2)
+        if ((i+1)%self.L,j) in self.p:
+            s2 = self.s[(i+1)%self.L][(j+1)%self.L]
+            s3 = self.s[(i+2)%self.L][j]
+            s4 = self.s[(i+1)%self.L][(j-1)%self.L]
+            s5 = self.s[(i-1)%self.L][(j-1)%self.L]
+            s6 = self.s[(i-2)%self.L][j]
+            s7 = self.s[(i-1)%self.L][(j+1)%self.L]
+        else:
+            s2 = self.s[(i-1)%self.L][(j+1)%self.L]
+            s3 = self.s[i][(j+2)%self.L]
+            s4 = self.s[(i+1)%self.L][(j+1)%self.L]
+            s5 = self.s[(i+1)%self.L][(j-1)%self.L]
+            s6 = self.s[i][(j-2)%self.L]
+            s7 = self.s[(i-1)%self.L][(j-1)%self.L]
+
+        dE = 2*self.I*s1*(s2*s3*s4 + s5*s6*s7)
 
         return dE
 
@@ -446,6 +453,5 @@ def test_model(test_model):
     plt.xlabel("Temperate (T)")
     plt.ylabel("Interaction strength (I)")
     plt.show()
-
 
 
